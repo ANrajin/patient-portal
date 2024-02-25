3353f22ebfa956dd056e488afd6407a238e8fd4f
anrajin <an.rajin@gmail.com>
2024-02-25 19:39:43 +0600

    [new] - project template initialized

---
+++ src/PatientPortal.Api/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/weatherforecast", () =>
+{
+    return Results.Ok();
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();

6503fad0b20a4c3d5d869e47ee395beabb3ef4b5
anrajin <an.rajin@gmail.com>
2024-02-26 13:18:01 +0600

    [update] - move selected option via js

--- src/PatientPortal.Web/wwwroot/js/site.js
+++ src/PatientPortal.Web/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $("#addAllergies").on('click', function (e) {
+        e.preventDefault();
+        moveSelectedOptions('select[name=allergies]', 'select[name=selectedAllergies]');
+    });
 
-// Write your JavaScript code.
+    $("#removeAllergies").on('click', function (e) {
+        e.preventDefault();
+        moveSelectedOptions('select[name=selectedAllergies]', 'select[name=allergies]');
+    });
+
+    $("#addNcds").on('click', function (e) {
+        e.preventDefault();
+        moveSelectedOptions('select[name=ncds]', 'select[name=selectedNcds]');
+    });
+
+    $("#removeNcds").on('click', function (e) {
+        e.preventDefault();
+        moveSelectedOptions('select[name=selectedNcds]', 'select[name=ncds]');
+    });
+
+    function moveSelectedOptions(from, to) {
+        const selectedOptions = $(from).find('option:selected');
+
+        selectedOptions.clone().appendTo(to);
+
+        selectedOptions.remove();
+    }
+});

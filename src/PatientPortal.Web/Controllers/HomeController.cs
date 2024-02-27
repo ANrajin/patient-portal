87db93b845709e32125b500bcbf778bcb620c905
anrajin <an.rajin@gmail.com>
2024-02-27 23:04:13 +0600

    [update] - controller action restructured

--- src/PatientPortal.Web/Controllers/HomeController.cs
+++ src/PatientPortal.Web/Controllers/HomeController.cs
@@ -1,78 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using PatientPortal.Web.Models;
-using PatientPortal.Web.Models.HomeModels;
 using System.Diagnostics;
 
 namespace PatientPortal.Web.Controllers
 {
-    public class HomeController(IServiceScopeFactory serviceScopeFactory,
-        ILogger<HomeController> logger) : Controller
+    public class HomeController(ILogger<HomeController> logger) : Controller
     {
         private readonly ILogger<HomeController> _logger = logger;
 
         public async Task<IActionResult> Index()
         {
             return View();
-        }
-
-        public async Task<IActionResult> Create()
-        {
-            try
-            {
-                using var serviceScope = serviceScopeFactory.CreateScope();
-
-                var model = serviceScope.ServiceProvider.GetRequiredService<HomeModel>();
-
-                await model.LoadModelData();
-
-                return View(model);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message, ex);
-
-                return RedirectToAction("Error");
-            }
-        }
-
-        public async Task<IActionResult> Patients(int id)
-        {
-            try
-            {
-                using var serviceScope = serviceScopeFactory.CreateScope();
-
-                var model = serviceScope.ServiceProvider.GetRequiredService<PatientViewModel>();
-
-                await model.LoadModelData(id);
-
-                return View(model);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message, ex);
-
-                return RedirectToAction("Error");
-            }
-        }
-
-        public async Task<IActionResult> Edit(int id)
-        {
-            try
-            {
-                using var serviceScope = serviceScopeFactory.CreateScope();
-
-                var model = serviceScope.ServiceProvider.GetRequiredService<PatientEditModel>();
-
-                await model.LoadModelData(id);
-
-                return View(model);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message, ex);
-
-                return RedirectToAction("Error");
-            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ src/PatientPortal.Web/Controllers/PatientsController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientPortal.Web.Models.HomeModels;
+
+namespace PatientPortal.Web.Controllers
+{
+    public class PatientsController(IServiceScopeFactory serviceScopeFactory,
+        ILogger<HomeController> logger) : Controller
+    {
+        private readonly ILogger<HomeController> _logger = logger;
+
+        public async Task<IActionResult> Create()
+        {
+            try
+            {
+                using var serviceScope = serviceScopeFactory.CreateScope();
+
+                var model = serviceScope.ServiceProvider.GetRequiredService<HomeModel>();
+
+                await model.LoadModelData();
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        public async Task<IActionResult> View(int id)
+        {
+            try
+            {
+                using var serviceScope = serviceScopeFactory.CreateScope();
+
+                var model = serviceScope.ServiceProvider.GetRequiredService<PatientViewModel>();
+
+                await model.LoadModelData(id);
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            try
+            {
+                using var serviceScope = serviceScopeFactory.CreateScope();
+
+                var model = serviceScope.ServiceProvider.GetRequiredService<PatientEditModel>();
+
+                await model.LoadModelData(id);
+
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+
+                return RedirectToAction("Error", "Home");
+            }
+        }
+    }
+}
